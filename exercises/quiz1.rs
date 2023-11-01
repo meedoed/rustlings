29e99580b6c559c129bd65fee4329ad2c9a3fb16
a.ulyanov <a.ulyanov@npo-echelon.ru>
2023-11-01 18:16:35 +0300

    done if, quiz1 and primitive types

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,12 +13,19 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn calculate_price_of_apples {
 
 // Don't modify this function!
+
+fn calculate_price_of_apples(count: u32) -> u32 {
+    if count > 40 {
+        count * 1
+    } else {
+        count * 2
+    }
+}
 #[test]
 fn verify_test() {
     let price1 = calculate_price_of_apples(35);
